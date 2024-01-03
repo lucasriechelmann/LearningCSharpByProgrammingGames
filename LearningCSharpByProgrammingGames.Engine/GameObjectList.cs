b586069e8ae23d9d83dedbf1ee4b7a464c80811f
Lucas Riechelmann Ramos <lucas.riechelmann@hotmail.com>
2024-01-03 21:14:23 +0000

    updated game object and game object list

--- LearningCSharpByProgrammingGames.Engine/GameObjectList.cs
+++ LearningCSharpByProgrammingGames.Engine/GameObjectList.cs
@@ -11,42 +11,46 @@ public class GameObjectList : GameObject
     /// <summary>
     /// The child objects of this game object.
     /// </summary>
-    public List<GameObject> _list;
+    List<GameObject> children;
+
     /// <summary>
     /// Creates a new GameObjectList with an empty list of children.
     /// </summary>
-
     public GameObjectList()
     {
-        _list = new();
+        children = new List<GameObject>();
     }
+
     /// <summary>
     /// Adds an object to this GameObjectList, and sets this GameObjectList as the parent of that object.
     /// </summary>
     /// <param name="obj">The game object to add.</param>
-    public void AddChild(GameObject child)
+    public void AddChild(GameObject obj)
     {
-        child.Parent = this;
-        _list.Add(child);
+        obj.Parent = this;
+        children.Add(obj);
     }
+
     /// <summary>
     /// Performs input handling for all game objects in this GameObjectList.
     /// </summary>
     /// <param name="inputHelper">An object required for handling player input.</param>
     public override void HandleInput(InputHelper inputHelper)
     {
-        for (int i = _list.Count - 1; i >= 0; i--)
-            _list[i].HandleInput(inputHelper);
+        for (int i = children.Count - 1; i >= 0; i--)
+            children[i].HandleInput(inputHelper);
     }
+
     /// <summary>
     /// Performs the Update method for all game objects in this GameObjectList.
     /// </summary>
     /// <param name="gameTime">An object containing information about the time that has passed in the game.</param>
     public override void Update(GameTime gameTime)
     {
-        foreach (GameObject obj in _list)
+        foreach (GameObject obj in children)
             obj.Update(gameTime);
     }
+
     /// <summary>
     /// Performs the Draw method for all game objects in this GameObjectList.
     /// </summary>
@@ -57,15 +61,16 @@ public class GameObjectList : GameObject
         if (!Visible)
             return;
 
-        foreach (GameObject obj in _list)
+        foreach (GameObject obj in children)
             obj.Draw(gameTime, spriteBatch);
     }
+
     /// <summary>
     /// Performs the Reset method for all game objects in this GameObjectList.
     /// </summary>
     public override void Reset()
     {
-        foreach (GameObject obj in _list)
+        foreach (GameObject obj in children)
             obj.Reset();
     }
 }
